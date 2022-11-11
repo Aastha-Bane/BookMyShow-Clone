1c6b7eddcb98c27239d93ad45b2b2f8da1cc7635
Aastha H. Bane <aastha.bane121@gmail.com>
2022-11-11 17:54:02 +0530

    First commit

---
+++ src/config/TempPosters.config.js
@@ -0,0 +1,51 @@
+const PremierImages = [{
+        src: "https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@bms_premiere_v1.png,oit-false,ofo-bottom_left:q-80/et00076943-kxbgajbmzr-portrait.jpg",
+        alt: "zack",
+        title: "Zacl",
+        subtitle: "english",
+    },
+    {
+        src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00047164-sztaxpptsc-portrait.jpg",
+        alt: "zack",
+        title: "Zacl",
+        subtitle: "english",
+    },
+    {
+        src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00047164-sztaxpptsc-portrait.jpg",
+        alt: "zack",
+        title: "Zacl",
+        subtitle: "english",
+    },
+    {
+        src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00047164-sztaxpptsc-portrait.jpg",
+        alt: "zack",
+        title: "Zacl",
+        subtitle: "english",
+    },
+    {
+        src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00047164-sztaxpptsc-portrait.jpg",
+        alt: "zack",
+        title: "Zacl",
+        subtitle: "english",
+    },
+    {
+        src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00047164-sztaxpptsc-portrait.jpg",
+        alt: "zack",
+        title: "Zacl",
+        subtitle: "english",
+    },
+    {
+        src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00047164-sztaxpptsc-portrait.jpg",
+        alt: "zack",
+        title: "Zacl",
+        subtitle: "english",
+    },
+    {
+        src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00047164-sztaxpptsc-portrait.jpg",
+        alt: "zack",
+        title: "Zacl",
+        subtitle: "english",
+    },
+];
+
+export default PremierImages;
